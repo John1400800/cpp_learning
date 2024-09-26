be7943a8c319dcbc39918c39f7116bef05edb616
john <johserheiv@gmail.com>
2024-09-26 22:35:10 +0300

    updated

--- ch20FunctionPointers/t01func_ptr.cpp
+++ ch20FunctionPointers/t01func_ptr.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <cstdint>
+#include <cstring>
 #include <iterator>
 #include <iostream>
 
@@ -11,7 +12,7 @@ void swap(T& first, T& second) {
 }
 
 template <typename T>
-void selSort(T *begin, const T *end, bool (*compFunc)(T&, T&)) {
+void selSort(T *begin, const T *end, bool (*compFunc)(const T&, const T&)) {
     for (auto startIt{begin}; startIt!=end; ++startIt) {
         auto swapElemIt{startIt};
         for (auto currIt{startIt+1}; currIt!=end; ++currIt)
@@ -22,12 +23,25 @@ void selSort(T *begin, const T *end, bool (*compFunc)(T&, T&)) {
 }
 
 template <typename T>
-bool ascending(T& first, T& second) {
+bool ascending(const T& first, const T& second) {
     return first > second;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream& out, const T (&arr)[N]) {
+    for (size_t i{0}; i<N; ++i)
+        std::cout << (i!=0?", ":"") << arr[i];
+    return out;
+}
+
+std::ostream& operator<<(std::ostream& out, const char* arr) {
+    return out.write(arr, static_cast<std::streamsize>(std::strlen(arr)));
 }
 
 int main() {
     uint32_t arr[]{ 2, 5, 3, 1, 4 };
+    std::cout << arr << '\n';
     selSort(std::begin(arr), std::end(arr), ascending);
+    std::cout << arr << '\n';
     return EXIT_SUCCESS;
 }
