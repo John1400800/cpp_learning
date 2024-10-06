fea1067284b7f3ab9e2a1d677b9175c9b332d8a5
john <johserheiv@gmail.com>
2024-10-06 15:42:39 +0300

    updated

--- ch22MoveSemanticsAndSmartPointers/auto_ptr/main.cpp
+++ ch22MoveSemanticsAndSmartPointers/auto_ptr/main.cpp
@@ -1,40 +1,65 @@
+// main.cpp
 #include <cstdlib>
 #include <cstdint>
 #include <array>
-#include <iostream>
 #include <functional>
+#include <iostream>
 
-#include "auto_ptr.h"
 #include "elem.h"
-#include "utils.h"
 
-constexpr size_t len{ 4 };
-constexpr std::array<uint32_t, len> iniArr{ 4, 1, 3, 2 };
+// #define USE_UTILS
+// #define USE_AUTO_PTR
+
+#ifdef USE_UTILS
+#include "utils.h"
+#else
+#include <algorithm>
+#endif
+
+#ifdef USE_AUTO_PTR
+#include "auto_ptr.h"
+template<typename T>
+using SmrtPtr = Auto_ptr<T>;
+#else
+#include <memory>
+template<typename T>
+using SmrtPtr = std::unique_ptr<T>;
+#endif
+
+template <typename T, typename Compare>
+void sort(T* begin, T* end, Compare comp) {
+#ifdef USE_UTILS
+    selSort(begin, end, comp);
+#else
+    std::sort(begin, end, comp);
+#endif
+}
 
 template <typename T, size_t N>
-std::ostream& operator<<(std::ostream& out, Auto_ptr<Elem<T>> (&arr)[N]) {
+std::ostream& operator<<(std::ostream& out, const std::array<SmrtPtr<Elem<T>>, N>& arr) {
     for (size_t i{0}; i < N; ++i)
-        std::cout << (i > 0 ? ", " : "") << *arr[i];
+        out << (i > 0 ? ", " : "") << *arr[i];
     return out;
 }
 
-int main() {
-    Auto_ptr<Elem<uint32_t>> arr[len];
-    for (size_t i{0}; i < len; ++i)
-        arr[i] = Auto_ptr<Elem<uint32_t>>{ new Elem<uint32_t>{iniArr[i]} };
+constexpr size_t len{4};
+constexpr std::array<uint32_t, len> iniArr{4, 1, 3, 2};
 
-    std::cout << arr << '\n';
-    
-    std::function<bool(const Auto_ptr<Elem<uint32_t>>&,
-                       const Auto_ptr<Elem<uint32_t>>&)> comp {
-        [](const Auto_ptr<Elem<uint32_t>>& a,
-           const Auto_ptr<Elem<uint32_t>>& b) {
-            return *a < *b;
-        }};
-    
-    // Передаем указатели на массив как итераторы
-    selSort(arr, arr + len, comp);
-    
-    std::cout << arr << '\n';
+int main() {
+    std::array<SmrtPtr<Elem<uint32_t>>, len> arr;
+    for (size_t i{0}; i < len; ++i)
+        arr[i] = SmrtPtr<Elem<uint32_t>>{new Elem<uint32_t>{iniArr[i]}};
+
+    std::cout << "Before sorting: " << arr << '\n';
+
+    // Лямбда для сравнения
+    auto comp = [](const SmrtPtr<Elem<uint32_t>>& a, const SmrtPtr<Elem<uint32_t>>& b) {
+        return *a < *b;
+    };
+
+    // Вызов функции сортировки
+    sort(arr.data(), arr.data() + arr.size(), comp);
+
+    std::cout << "After sorting: " << arr << '\n';
     return EXIT_SUCCESS;
 }
--- ch22MoveSemanticsAndSmartPointers/auto_ptr/utils.h
+++ ch22MoveSemanticsAndSmartPointers/auto_ptr/utils.h
@@ -1,12 +1,30 @@
+// utils.h
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <utility>
 #include <functional>
 
 template <typename T>
-void swap(T&, T&);
+void swap(T& a, T& b) {
+    T temp = std::move(a);
+    a = std::move(b);
+    b = std::move(temp);
+}
 
-template <typename T>
-void selSort(T*, T*, std::function<bool(const T&, const T&)>);
+template <typename T, typename Compare>
+void selSort(T* begin, T* end, Compare comp) {
+    for (T* i = begin; i != end; ++i) {
+        T* minElem = i;
+        for (T* j = i + 1; j != end; ++j) {
+            if (comp(*j, *minElem)) {
+                minElem = j;
+            }
+        }
+        if (minElem != i) {
+            ::swap(*i, *minElem);
+        }
+    }
+}
 
 #endif // UTILS_H
